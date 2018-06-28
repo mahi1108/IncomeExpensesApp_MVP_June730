41c26385a1fb438c99abd6522489a0211d34a2e8
maheshthippala <mahesh.cubex@gmail.com>
2018-06-28 22:37:34 +0530

    Added remaining functionality.

--- app/src/main/java/cubex/mahesh/incomeexpensesapp_mvp/model/IncExpModel.kt
+++ app/src/main/java/cubex/mahesh/incomeexpensesapp_mvp/model/IncExpModel.kt
@@ -1,28 +1,106 @@
 package cubex.mahesh.incomeexpensesapp_mvp.model
 
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
 import cubex.mahesh.incomeexpensesapp_mvp.beans.IncExpBean
 import cubex.mahesh.incomeexpensesapp_mvp.presenter.IncExpPresenterAPI
+import cubex.mahesh.incomeexpensesapp_mvp.view.IncExpViewApi
+import cubex.mahesh.incomeexpensesapp_mvp.view.MainActivity
 
 class IncExpModel : IncExpPresenterAPI
 {
+    lateinit  var dBase:SQLiteDatabase
+
+    lateinit  var mActivity : MainActivity
+
+    constructor(view_api:IncExpViewApi)
+    {
+        /* IncExpViewApi view_api = new MainActivity( );
+            MainActivity   mActivity = (MainActivity)view_api */
+         mActivity= view_api as MainActivity
+            dBase = mActivity.openOrCreateDatabase("incexp_db",
+                    Context.MODE_PRIVATE,null)
+        dBase.execSQL("create table if not exists incexp(_id integer primary key autoincrement,type varchar(15),money integer,description varchar(1000)) ")
+    }
+
     override fun addInc(bean: IncExpBean) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        var cv = ContentValues( )
+        cv.put("type",bean.type)
+        cv.put("money",bean.money)
+        cv.put("description",bean.desc)
+
+   var status =  dBase.insert("incexp",null,
+           cv)
+   lateinit var msg:String
+        if(status==-1.toLong()){
+            msg = "Record Insertion Failed"
+        }else{
+            msg = "Record Insertion Success"
+        }
+        mActivity.addIncResult(msg)
     }
 
     override fun addExp(bean: IncExpBean) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        var cv = ContentValues( )
+        cv.put("type",bean.type)
+        cv.put("money",bean.money)
+        cv.put("description",bean.desc)
+
+        var status =  dBase.insert("incexp",null,
+                cv)
+        lateinit var msg:String
+        if(status==-1.toLong()){
+            msg = "Record Insertion Failed"
+        }else{
+            msg = "Record Insertion Success"
+        }
+        mActivity.addExpResult(msg)
     }
 
     override fun viewExp() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+   var c = dBase.query("incexp",null,
+                    "type=?",
+                    arrayOf("Expense"),null,
+                    null,null)
+        var list = mutableListOf<IncExpBean>()
+        while (c.moveToNext()){
+            var bean = IncExpBean(c.getInt(0),
+                    c.getString(1),c.getInt(2),
+                    c.getString(3))
+            list.add(bean)
+        }
+        mActivity.viewExpResult(list)
     }
 
     override fun viewInc() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        var c = dBase.query("incexp",null,
+                "type=?",
+                arrayOf("Income"),null,
+                null,null)
+        var list = mutableListOf<IncExpBean>()
+        while (c.moveToNext()){
+            var bean = IncExpBean(c.getInt(0),
+                    c.getString(1),c.getInt(2),
+                    c.getString(3))
+            list.add(bean)
+        }
+        mActivity.viewIncResult(list)
     }
 
     override fun viewIncExp() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        var c = dBase.query("incexp",null,
+                null,
+                null,null,
+                null,null)
+        var list = mutableListOf<IncExpBean>()
+        while (c.moveToNext()){
+            var bean = IncExpBean(c.getInt(0),
+                    c.getString(1),c.getInt(2),
+                    c.getString(3))
+            list.add(bean)
+        }
+        mActivity.viewIncExpResult(list)
     }
 
 }
--- app/src/main/java/cubex/mahesh/incomeexpensesapp_mvp/view/MainActivity.kt
+++ app/src/main/java/cubex/mahesh/incomeexpensesapp_mvp/view/MainActivity.kt
@@ -2,32 +2,81 @@ package cubex.mahesh.incomeexpensesapp_mvp.view
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import cubex.mahesh.incomeexpensesapp_mvp.R
 import cubex.mahesh.incomeexpensesapp_mvp.beans.IncExpBean
+import cubex.mahesh.incomeexpensesapp_mvp.model.IncExpModel
+import cubex.mahesh.incomeexpensesapp_mvp.presenter.IncExpPresenterAPI
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : IncExpViewApi,AppCompatActivity() {
     override fun addIncResult(msg: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            Toast.makeText(this,
+                    msg,Toast.LENGTH_LONG).show()
     }
 
     override fun addExpResult(msg: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        Toast.makeText(this,
+                msg,Toast.LENGTH_LONG).show()
     }
 
     override fun viewExpResult(list: MutableList<IncExpBean>) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        var temp_list = mutableListOf<String>()
+        for(bean in list){
+            temp_list.add("${bean._id}   ${bean.type} ${bean.money} ${bean.desc}")
+        }
+        var adapter = ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_single_choice,temp_list)
+        lview.adapter = adapter
+
     }
 
     override fun viewIncResult(list: MutableList<IncExpBean>) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        var temp_list = mutableListOf<String>()
+        for(bean in list){
+            temp_list.add("${bean._id}   ${bean.type} ${bean.money} ${bean.desc}")
+        }
+        var adapter = ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_single_choice,temp_list)
+        lview.adapter = adapter
     }
 
     override fun viewIncExpResult(list: MutableList<IncExpBean>) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+        var temp_list = mutableListOf<String>()
+        for(bean in list){
+            temp_list.add("${bean._id}   ${bean.type} ${bean.money} ${bean.desc}")
+        }
+        var adapter = ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_single_choice,temp_list)
+        lview.adapter = adapter
+   }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+var model:IncExpPresenterAPI = IncExpModel(this);
+// IncExpPresenterAPI api = new IncExpModel( ); UpCasting
+
+        add_inc.setOnClickListener({
+           var ie_bean = IncExpBean(0,sp1.selectedItem.toString(),
+                   money.text.toString().toInt(),desc.text.toString())
+                model.addInc(ie_bean)
+        })
+        add_exp.setOnClickListener({
+            var ie_bean = IncExpBean(0,sp1.selectedItem.toString(),
+                    money.text.toString().toInt(),desc.text.toString())
+            model.addExp(ie_bean)
+        })
+        view_inc.setOnClickListener({
+            model.viewInc()
+        })
+        view_exp.setOnClickListener({
+            model.viewExp()
+        })
+        inc_exp.setOnClickListener({
+            model.viewIncExp()
+        })
     }
 }
